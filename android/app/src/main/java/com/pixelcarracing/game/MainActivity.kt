33f6b2afd121c7c85689d2aaf5743a545881314d
Leo <somaratne.leo@gmail.com>
2026-05-08 13:18:02 +0000

    Add native Android app (WebView-based, works offline)

---
+++ android/app/src/main/java/com/pixelcarracing/game/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.pixelcarracing.game
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.settings.apply {
+            javaScriptEnabled = true
+            allowFileAccess = true
+            domStorageEnabled = true
+            setSupportZoom(false)
+            builtInZoomControls = false
+            displayZoomControls = false
+            loadWithOverviewMode = true
+            useWideViewPort = true
+            allowContentAccess = true
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
